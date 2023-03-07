e84bdb24e98981ce854dbca778ee3be022efb360
kpetrosky <kpetrosky36@gmail.com>
2023-03-06 20:33:31 -0700

    added the link between html and javascript

--- script.js
+++ script.js
@@ -6,63 +6,30 @@
 // git status (checks the branch you are on )
 // git switch {name of branch} (will tell you the name of your branch)
 
-
-// var apiKey
-// $.get('http://numbersapi.com/1337/trivia?notfound=floor&fragment', function(data) {
-//     $('#number').text(data);
-// });
-
+//function call each api
+// separate api each function
+//init to call multiple 
 
 
 var url = "http://numbersapi.com/number/type";
 var random = document.getElementById("random");
-// var factDisplay = document.getElementById("fact-display");
-
-// factButton.addEventListener("click", async () => {
-//     .then {
-//         var response = await fetch(url);
-//         var imageData = await response.data();
-//         console.log(imageData);
-//         // Do something with the image data, e.g. display it in an <img> element
-//         // factDisplay.src = URL.createObjectURL(imageData);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// });
 
 fetch('http://numbersapi.com/number/trivia')
-    .then(function(response) {
+    .then(function (response) {
         console.log(response);
         return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
         console.log(data);
     })
-    .catch(function(error) {
+    .catch(function (error) {
         console.error(error);
     });
 
 
-
-// var factButton = document.getElementById("fact-button");
-// var factDisplay = document.getElementById("fact-display");
-
-// fetch('https://go-apod.herokuapp.com/apod')
-//   .then(function(response) {
-//     // return response.text();
-//   })
-//   .then(function(data) {
-//     console.log(data);
-//   })
-//   .catch(function(error) {
-//     console.error(error);
-//   });
-// //   (function(factDisplay){
-// //   console.log(data)
-// //   })
+getApi()
 // factButton.addEventListener("click", async () => {
 //   var response = await fetch("https://go-apod.herokuapp.com/apod");
 //   var fact = await response.text();
 //   factDisplay.innerText = fact;
 // });
-// function getApi();
