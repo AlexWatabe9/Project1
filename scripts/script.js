be58ef046daf5ab28ee236329f219aa04fdfc906
AlexWatabe9 <alexwatabe@protonmail.com>
2023-03-06 20:35:32 -0700

    pulling from main

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function getApi() {
+  var requestApi = "https://imgflip.com/api"
+  fetch(requestApi).then(function (response) {
+    return response.json();
+    
+  })
+  .then(function (data) {
+    return console.log(data);
+  });
+
+}
+getApi();
