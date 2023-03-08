652a0354a77a97eea7a2121725a453da6467662f
kpetrosky <kpetrosky36@gmail.com>
2023-03-07 18:09:00 -0700

    figured out how to attach an api and get the console to read it with no errors

--- script.js
+++ script.js
@@ -1,55 +1,31 @@
-// git add .
-// git commit -m ""
-// git push origin {branch name} 
 
-
-// git status (checks the branch you are on )
-// git switch {name of branch} (will tell you the name of your branch)
 
 //function call each api
 // separate api each function
 //init to call multiple 
 
 
-// var url = "http://numbersapi.com/number/type";
+var apiKey = '158f9ecd45575b4b5ba983959881d8f3';
+var artist = 'Pink Floyd';
+var track = 'Comfortably Numb';
+var apiUrl = `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${apiKey}&artist=${artist}&track=${track}&format=json`;
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+//   getApi();
 
 
-var random = document.getElementById("random");
-
-function getApi() {
-    var url = "http://numbersapi.com/42/trivia";
-
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
-}
-
-getApi();
-
-
-// fetch(url)
-//     .then(function (response) {
-//         console.log(response);
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//     })
-//     .catch(function (error) {
-//         console.error(error);
-//     });
-
-
-
-// factButton.addEventListener("click", async () => {
-//   var response = await fetch("https://go-apod.herokuapp.com/apod");
-//   var fact = await response.text();
-//   factDisplay.innerText = fact;
-// });
+document.getElementById('Facts').addEventListener('click', () => {
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Do something with the data here
+    //   console.log(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+});
+  
--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,41 @@
-function getApi() {
+// function getApi() {
 
-    fetch("https://imgflip.com/api", {
-      mode: 'no-cors'
-    })
+//     fetch("https://imgflip.com/api", {
+//       mode: 'no-cors'
+//     })
     
-    .then(function (response) {
-      return response.json();
+//     .then(function (response) {
+//       return response.json();
       
-    })
-    .then(function (data) {
-      return console.log(data);
-    });
+//     })
+//     .then(function (data) {
+//       return console.log(data);
+//     });
   
-  }
-  getApi();
+//   }
+//   getApi();
+
+  
+var apiKey = '158f9ecd45575b4b5ba983959881d8f3';
+var artist = 'Pink Floyd';
+var track = 'Comfortably Numb';
+var apiUrl = `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${apiKey}&artist=${artist}&track=${track}&format=json`;
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+//   getApi();
+
+
+document.getElementById('Facts').addEventListener('click', () => {
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Do something with the data here
+    //   console.log(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+});
